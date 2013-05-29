ef4d6f596cd0d78dd88ef0c545973f14b44e1d9e
Kirby Cool <kirbydcool@gmail.com>
2013-05-28 21:17:57 -0700

    added to lighting model

--- Engine/light.js
+++ Engine/light.js
@@ -1,6 +1,7 @@
-ENGINE.Light = function( position, color ) {
+ENGINE.Light = function( position, color, intensity ) {
   this.position = position !== undefined ? vec3.fromValues( 0.0, 0.0, 0.0 );
   this.color = color !== undefined ? vec4.fromValues( 1.0, 1.0, 1.0, 1.0 );
+  this.intensity = intensity !== undefined ? 1.0;
 }
 
-
+ENGINE.Light.prototype = {};
