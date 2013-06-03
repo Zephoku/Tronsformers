6debd06876f9ca868dc85110e15cbe1a38ff7467
Robert Nguyen <roberttnb@gmail.com>
2013-06-03 14:18:16 -0700

    fixed head bugs

--- js/track.js
+++ js/track.js
@@ -10,7 +10,6 @@ var cameraX = 0.0;
 var cameraY = 3.0;
 var cameraZ = 9.0;
 
-
 // Custom Messages
 statusMessages = {
   "getUserMedia" : "getUserMedia seems to be supported",
@@ -75,6 +74,28 @@ function updateFaceDebugMessages(event) {
   var messagep = document.getElementById('headtrackerAngle');
   messagep.innerHTML = event.angle * 180/Math.PI;
 }
+
+function updateCameraMovement(event){
+  cameraX = (170 - event.x) *.001;
+  cameraY = (145 - event.y) *.001;
+  cameraZ = event.height/event.width * 5;
+}
+
+// Face Detection
+document.addEventListener("facetrackingEvent", function( event ) {
+  // Clear Canvas
+  overlayContext.clearRect(0,0,320,240);
+
+  // Check for stable detection
+  if (event.detection == "CS") {
+    // Debug callbacks
+    drawFaceRectangle(event);
+    updateFaceDebugMessages(event);
+
+    // Camera movement
+    updateCameraMovement(event);
+  }
+});
 
 // turn off or on the canvas showing probability
 function showProbabilityCanvas() {
